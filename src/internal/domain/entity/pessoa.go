5893cd1468ab0b81af914527329474025bd45605
Rogerio Tadim <rogeriotadim@gmail.com>
2023-09-24 16:13:44 -0300

    app: pessoa fix validation

--- src/internal/domain/entity/pessoa.go
+++ src/internal/domain/entity/pessoa.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 
@@ -17,32 +18,9 @@ type Pessoa struct {
 }
 
 func NewPessoa(apelido, nome, nascimento string, stack []string) (pessoa *Pessoa, err error) {
-	if apelido == "" {
-		return nil, errors.New("apelido inválido")
-	}
-
-	if nome == "" {
-		return nil, errors.New("nome inválido")
-	}
-
-	if nascimento == "" {
-		return nil, errors.New("nascimento inválido")
-	}
-
-	data := strings.Split(nascimento, "-")
-	ano, err := strconv.Atoi(data[0])
-	if err != nil || ano < 1 || ano > 2023 {
-		return nil, errors.New("ano inválido")
-	}
-
-	mes, err := strconv.Atoi(data[1])
-	if err != nil || mes < 1 || mes > 12 {
-		return nil, errors.New("mês inválido")
-	}
-
-	dia, err := strconv.Atoi(data[2])
-	if err != nil || dia < 1 || dia > 31 {
-		return nil, errors.New("dia inválido")
+	err = validate(apelido, nome, nascimento, stack)
+	if err != nil {
+		return nil, err
 	}
 
 	return &Pessoa{
@@ -52,4 +30,48 @@ func NewPessoa(apelido, nome, nascimento string, stack []string) (pessoa *Pessoa
 		Nascimento: nascimento,
 		Stack:      stack,
 	}, nil
+}
+
+func validate(apelido, nome, nascimento string, stack []string) error {
+	tipo, err := fmt.Printf("%T", apelido)
+	if err != nil || apelido == "" || tipo != 6 {
+		return errors.New("apelido inválido")
+	}
+
+	tipo, err = fmt.Printf("%T", nome)
+	if err != nil || nome == "" || tipo != 6 {
+		return errors.New("nome inválido")
+	}
+
+	tipo, err = fmt.Printf("%T", nascimento)
+	if err != nil || nascimento == "" || tipo != 6 {
+		return errors.New("nascimento inválido")
+	}
+
+	data := strings.Split(nascimento, "-")
+	ano, err := strconv.Atoi(data[0])
+	if err != nil || ano < 1 || ano > 2023 {
+		return errors.New("ano inválido")
+	}
+
+	mes, err := strconv.Atoi(data[1])
+	if err != nil || mes < 1 || mes > 12 {
+		return errors.New("mês inválido")
+	}
+
+	dia, err := strconv.Atoi(data[2])
+	if err != nil || dia < 1 || dia > 31 {
+		return errors.New("dia inválido")
+	}
+
+	if len(stack) > 0 {
+		for _, v := range stack {
+			tipo, err = fmt.Printf("%T", v)
+			if err != nil || tipo != 6 {
+				return errors.New("stack inválida")
+			}
+		}
+	}
+
+	return nil
 }
